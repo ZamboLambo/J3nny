dfeae4709a666599e61dd87647eff8af71a3fcef
ZamboLambo <92601355+ZamboLambo@users.noreply.github.com>
2022-10-16 20:39:20 -0300

    Add files via upload

---
+++ Archive-Spider.py
@@ -0,0 +1,84 @@
+from pyexpat import features
+from urllib.request import urlopen
+import requests
+from bs4 import BeautifulSoup
+import re
+import itertools
+import difflib
+from thread_parser import *
+
+thread_pattern = "mr-co"
+board = "co"
+sleep_minutes = 5
+minreplies = 8
+
+#Gets first thread link from archive based on pattern
+#Pattern is thread title or if theres none, body of text
+#4chan archives automatically remove special characters, make it lowercase and replace spaces with -
+#return value is string
+def get_thread(pattern,board): 
+    html = urlopen("https://boards.4channel.org/" + board + "/archive")
+    bsobj = BeautifulSoup(html, 'html.parser')
+    match = str(bsobj.find(href = re.compile(pattern)))
+    cut1 = re.split(pattern,match)
+    cut2 = re.split("href=\"",cut1[0])
+    thread = "https://boards.4channel.org" + cut2[1]
+    return thread
+
+def main():
+    while(True):
+        
+        try:
+            open("scraped_threads.txt")
+        except FileNotFoundError:
+            open("scraped_threads.txt",'x')
+        finally:
+            threads_list = []
+            f = open("scraped_threads.txt","r+")
+            fields = []
+            for line in f:
+                fields = line.split()
+
+            curr_thread = get_thread(thread_pattern,board)
+
+            print(fields)
+            if curr_thread not in fields: 
+                print("Beginning scrape routine of " + curr_thread)
+                f.write(curr_thread + "\n") #unscraped thread, append to list and scrape
+                f.close()
+                post_list = get_posts(curr_thread)
+                character_list = handle_posts(post_list,minreplies)
+
+                try:
+                    open("Nominations_list.txt")
+                except FileNotFoundError:
+                    open("Nominations_list.txt",'x')
+                finally:
+                    f = open("Nominations_list.txt",'r+')
+                    for item in character_list:
+                        f.write(item)
+                    print(str(len(character_list)) + " nominations added to nomination file.")
+                    for entry_a, entry_b in itertools.combinations(f,2):
+                        similarity_alpha = round(difflib.SequenceMatcher(lambda x: x == " ",entry_a,entry_b))
+                        if (similarity_alpha > 0.65): #remove repeats
+                            d = f.readlines()
+                            for item in d:
+                                if item != entry_a:
+                                    f.write(item)
+
+
+            else:
+                f.close() #scraped already, close file and do nothing
+
+
+
+
+        print("Going off for " + str(sleep_minutes) + " minutes")
+        sleep(sleep_minutes * 60)
+
+
+    return 0
+
+if __name__ == "__main__":
+    main()
+
---
+++ thread_parser.py
@@ -0,0 +1,71 @@
+from lib2to3.pgen2 import driver
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+import re
+
+globvar = 0
+
+def is_post_valid(post,minreplies):
+    global globvar
+    print(str(globvar) + " posts verified." )
+    globvar = globvar + 1
+    try:
+        post.find_element(By.TAG_NAME,"img")
+    except NoSuchElementException:
+        return False #no image attached, invalid
+    else:
+        try:
+            post.find_element(By.CLASS_NAME,'backlink')
+        except NoSuchElementException:
+            return False #no replies, invalid
+        else:
+            replies = post.find_element(By.CLASS_NAME,'backlink').text
+            reply_list = replies.split()
+            if(len(reply_list) < minreplies):
+                return False #not enough replies, invalid
+            post_message = post.find_element(By.CLASS_NAME,'postMessage').text
+            match = re.search("[Nn]ominating.*from.*[.]",post_message)
+            if match:
+                return True #fits the bill, passed all tests, valid post
+            else:
+                return False #doesnt fit the pattern, invalid
+
+
+def get_posts(link): #goes to link, returns list of post webelements
+    options = webdriver.FirefoxOptions()
+    options.add_argument("--headless")
+
+    driver = webdriver.Firefox(options=options)
+    driver.get(link)
+    sleep(5)
+    post_list = driver.find_elements(By.CLASS_NAME, 'postContainer')
+
+    #driver.close()
+    return post_list
+
+#
+def handle_posts(posts, minreplies): #takes the original post list, converts into valid string list to archive
+    valid_posts = []
+
+    for post in posts:
+        if is_post_valid(post,minreplies): #remove invalids from list
+            valid_posts.append(post)
+
+    item_list = []
+    for post in valid_posts:
+       postmessage = post.find_element(By.CLASS_NAME,'postMessage').text
+       templist = re.split(r"[Nn]ominating|[.]", postmessage) #MIND THE ENDING DOT
+       postmessage = templist[1] #now postmessage holds only "x from y"
+       postmessage = re.sub('from','(',postmessage)+')'#postmessage is now x( y)
+       item_list.append(postmessage)
+    
+
+    return item_list
+
+
+
+
+
+
