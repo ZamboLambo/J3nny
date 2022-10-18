1ef48ff5ea64c56dac5371a326c631fa3ebec6bf
ZamboLambo <92601355+ZamboLambo@users.noreply.github.com>
2022-10-18 18:09:58 -0300

    Update Archive-Spider.py

--- Archive-Spider.py
+++ Archive-Spider.py
@@ -14,15 +14,17 @@ minreplies = 8
 #Gets first thread link from archive based on pattern
 #Pattern is thread title or if theres none, body of text
 #4chan archives automatically remove special characters, make it lowercase and replace spaces with -
-#return value is string
+#return value is string or 0 if failed to find thread
 def get_thread(pattern,board): 
     html = urlopen("https://boards.4channel.org/" + board + "/archive")
     bsobj = BeautifulSoup(html, 'html.parser')
     match = str(bsobj.find(href = re.compile(pattern)))
     cut1 = re.split(pattern,match)
     cut2 = re.split("href=\"",cut1[0])
-    thread = "https://boards.4channel.org" + cut2[1]
-    return thread
+    if len(cut2) > 1:
+        thread = "https://boards.4channel.org" + cut2[1]
+        return thread
+    return 0
 
 #takes filename, removes repeat text entries from it
 def remove_similar(file):
@@ -58,7 +60,7 @@ def main():
 
             curr_thread = get_thread(thread_pattern,board)
 
-            if curr_thread not in fields: 
+            if curr_thread and curr_thread not in fields: 
                 print("Beginning scrape routine of " + curr_thread)
                 f.write(curr_thread + "\n") #unscraped thread, append to list and scrape
                 f.close()
