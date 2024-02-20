83c3e2d722b5eecfa3c410240d29c18ff46e8d0e
ZamboLambo <gerekuv@gmail.com>
2024-02-20 18:40:10 -0300

    moved gui to a class

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import ttk
-from tkinter.messagebox import showinfo
+from tkinter.messagebox import showerror, showinfo
+import requests
 
 def newWindow(texts):
     showinfo("Input info", texts)
@@ -10,36 +11,51 @@ def makeEntry(lname, var, master, infoText):
     lPat.grid()
     tButton = ttk.Button(master, text="?", command= lambda: newWindow(infoText), width=1)
     Button.grid(self=tButton, column=1, row=lPat.grid_info()['row'])
-    ttk.Entry(master, exportselection=0, textvariable=var).grid(padx=20, pady=5) #may want to alter later,
-                                                                                 #perhaps could return this
+    tEntry = ttk.Entry(master, exportselection=0, textvariable=var)
+    tEntry.grid(padx=20, pady=5)
+    return tEntry
+
+
+class gui:
+    #user inputs to consume
+    def __init__(self, frm):
+        self.threadPat = StringVar()
+        self.board = StringVar()
+        self.sheet = StringVar()
+        self.minReplies = StringVar()
+        self.endstamp = StringVar()
+        self.tPat = makeEntry("Thread pattern", self.threadPat, frm, 
+"A word or sentence present in the thread OP to be searched, must be unique. ie: Nominations"),
+
+        self.board = makeEntry("Board", self.board, frm, 
+        "Letter or word of the board to scrape. ie: v, a, trash"),
+
+        self.gSheet = makeEntry("Google Sheet", self.sheet, frm,
+        "Name of Google Sheets sheet to use, if inexistent it will be created. Uneeded if Google Sheets isn't set to be used."),
+
+        self.minRep = makeEntry("Minimum replies", self.minReplies, frm,
+        "Minimum replies for a nomination to be considered valid."),
+
+        self.almo = makeEntry("itsalmo.st", self.endstamp, frm, 
+        "Link of the itsalmo.st timer to use. Part after the / only.")
+
+
+        ttk.Button(frm, text="Quit", command=lambda: self.validate()).grid(padx=20, pady=5)
+    def validate(self):
+        showerror(self.threadPat.get(), self.threadPat.get())
+
+
+
 
 root = Tk()
 root.resizable(False, False)
 
 frm = ttk.Frame(root, padding=10,)
 frm.grid()
-#user inputs to consume
-threadPat = StringVar()
-board = StringVar()
-sheet = StringVar()
-minReplies = StringVar()
-endstamp = StringVar()
 
-makeEntry("Thread pattern", threadPat, frm, 
-"A word or sentence present in the thread OP to be searched, must be unique. ie: Nominations")
 
-makeEntry("Board", board, frm, 
-"Letter or word of the board to scrape. ie: v, a, trash")
+gui = gui(root)
 
-makeEntry("Google Sheet", sheet, frm,
-"Name of Google Sheets sheet to use, if inexistent it will be created. Uneeded if Google Sheets isn't set to be used.")
 
-makeEntry("Minimum replies", minReplies, frm,
-"Minimum replies for a nomination to be considered valid.")
-
-makeEntry("itsalmo.st", endstamp, frm, 
-"Link of the itsalmo.st timer to use. Part after the / only.")
-
-ttk.Button(frm, text="Quit", command=root.destroy).grid(padx=20, pady=5)
 
 root.mainloop()
